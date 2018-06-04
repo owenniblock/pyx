54cbe95d5e9d91e1079182f68d80922827060b40
owenniblock <info@kumquatcomputing.co.uk>
2018-06-04 13:34:40 +0100

    Adds stub controllers for other PoC functionality

---
+++ Api/Controllers/ReactionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pyx.Api.Models;
+
+namespace Pyx.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReactionController : Controller
+    {
+        // POST api/reaction
+        [HttpPost]
+        public void Post([FromBody]Reaction value)
+        {
+        }
+    }
+}
